4b4bff85b03e6f5871e60b00513bc1bb8521f6de
MiquelENTI <miquel.forcada@enti.cat>
2024-02-07 18:06:16 +0100

    Photo Movement Done

--- FragmentsOfThePast/Assets/Scripts/Minigames/DevelopPhotos/DragableUIObject.cs
+++ FragmentsOfThePast/Assets/Scripts/Minigames/DevelopPhotos/DragableUIObject.cs
@@ -6,28 +6,47 @@ using UnityEngine.EventSystems;
 public class DragablePhoto : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     Vector2 startPoint;
-    Vector2 endPoint;
+    Vector2 cursorPosition;
 
     Rigidbody2D rb;
 
     [SerializeField] float maxRotation = 70.0f;
+
+    bool isCursorDragging = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (isCursorDragging)
+        {
+            Debug.Log(cursorPosition);
+
+            Vector3 vector = new Vector3(0, cursorPosition.y, 0) - new Vector3(0, transform.position.y, 0);
+
+            float distance = vector.magnitude;
+
+            Vector3 direction = vector.normalized;
+
+            rb.velocity = direction * distance;
+        }
+    }
+
     void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {
         startPoint = eventData.pressPosition;
-        endPoint = eventData.pressPosition;
+        cursorPosition = eventData.pressPosition;
     }
+
+    
 
     public void OnDrag(PointerEventData eventData)
     {
-        endPoint = eventData.position;
-
-        transform.position = endPoint;
+        isCursorDragging = eventData.dragging;
+        cursorPosition = eventData.position;
 
         rb.isKinematic = true;
     }
