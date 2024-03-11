95d6a85afd94df43147e718aa480d011f0660912
m0ki <miquel.forcada@enti.cat>
2024-03-11 19:45:41 +0100

    Fixed Trigger Issue in Build

--- FragmentsOfThePast/Assets/Scripts/Minigames/DevelopPhotos/CheckpointManager.cs
+++ FragmentsOfThePast/Assets/Scripts/Minigames/DevelopPhotos/CheckpointManager.cs
@@ -11,11 +11,11 @@ public class CheckpointManager : MonoBehaviour
     public float currentLaps = 0;
     public float totalLaps = 3;
 
-    DragablePhoto photo;
+    [SerializeField] DragablePhoto photo;
 
     void Start()
     {
-        photo = GameObject.FindGameObjectWithTag("Photo").GetComponent<DragablePhoto>();
+        //photo = GameObject.FindGameObjectWithTag("Photo").GetComponent<DragablePhoto>();
 
         currentCheckpoint = new();
 
